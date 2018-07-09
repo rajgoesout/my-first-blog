bc369e4e8f5b59a8bcfbb8461a4f73250afe8f7b
Rajdeep <rajdeepbharati13@gmail.com>
2018-07-09 21:44:35 +0530

    My Django app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Post # import the model Post from models.py
+
+admin.site.register(Post) # to make our model visible on the admin page, register the model
