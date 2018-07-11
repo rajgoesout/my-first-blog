3d496a9e66e389b27c4508f41788fc6b9e4cb11e
Rajdeep <rajdeepbharati13@gmail.com>
2018-07-11 11:35:21 +0530

    removed path to support Django 1.1

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
