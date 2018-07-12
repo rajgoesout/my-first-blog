c18fb806822b6c65cbf8389aaf9bfe15f3533b64
Rajdeep <rajdeepbharati13@gmail.com>
2018-07-12 21:05:49 +0530

    added feature to edit posts

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+
 # from django.urls import path
 
 urlpatterns = [
