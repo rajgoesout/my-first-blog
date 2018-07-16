92e63bfbfda2a9f1546b7aebd963b87e3512822c
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-07-16 21:38:37 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,7 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 
-# from django.urls import path
-
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
